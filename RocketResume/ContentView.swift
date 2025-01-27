0d346f866d379bffb317dee1a9fc9d7aa9303fad
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2025-01-27 12:10:49 -0300

    feat: Add resume sections

--- RocketResume/ContentView.swift
+++ RocketResume/ContentView.swift
@@ -9,11 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            Text("Rhuan Carvalho")
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            Text("Work Experience")
+                .font(.title2)
+            Rectangle()
+                .frame(height: 1)
+                .foregroundColor(.gray)
+            
+            Spacer()
+            
+            Text("Education")
+                .font(.title2)
+            Rectangle()
+                .frame(height: 1)
+                .foregroundColor(.gray)
+            
+            Spacer()
+            
+            Text("Interests")
+                .font(.title2)
+            Rectangle()
+                .frame(height: 1)
+                .foregroundColor(.gray)
+                .padding(.bottom)
+            HStack {
+                Spacer()
+                VStack {
+                    Text("🥋")
+                        .font(.system(size: 50))
+                    Text("Jiu Jitsu")
+                }
+                Spacer()
+                VStack {
+                    Text("🎮")
+                        .font(.system(size: 50))
+                    Text("Video Game")
+                }
+                Spacer()
+                VStack {
+                    Text("📱")
+                        .font(.system(size: 50))
+                    Text("App Dev")
+                }
+                Spacer()
+            }
         }
         .padding()
     }
